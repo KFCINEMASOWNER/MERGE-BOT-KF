19e2d5ec8eba9302beb67058fd36c018f3cbf573
KFCINEMASOWNER <ujwalnayak25@gmail.com>
2025-02-02 21:08:36 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,10 +2,10 @@ import os
 
 
 class Config(object):
-    API_HASH = os.environ.get("25953006")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN")
-    TELEGRAM_API = os.environ.get("TELEGRAM_API")
-    OWNER = os.environ.get("OWNER")
+    API_HASH = os.environ.get("d5850aeef7dd3d01fe6b698c0a0d4be8")
+    BOT_TOKEN = os.environ.get("7541410743:AAFBFnVNU3vkcUQwvCxf-rhlnHQSUJcv_wA")
+    TELEGRAM_API = os.environ.get("25953006")
+    OWNER = os.environ.get("1981280736")
     OWNER_USERNAME = os.environ.get("Ask_any_Movies_1")
     PASSWORD = os.environ.get("KFCINEMAS")
     DATABASE_URL = os.environ.get("DATABASE_URL")
